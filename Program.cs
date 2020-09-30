4e091d3088929c1be0c4996cb6105411a48c2bb2
RenoNovoa <djnova25@gmail.com>
2020-09-30 18:20:30 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Deliverable1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("^_^  Welcome to my first C# traveling Experience!  ^_^");
+            
+            Console.WriteLine();
+
+            Console.WriteLine("What kind of trip would you like to go on, musical, tropical, or adventurous ?");
+                
+            Console.WriteLine("Please Enter Your Choice:  ");
+            string vacationType = Console.ReadLine();
+
+
+            Console.Write("How many are in your group?");
+            int groupSize = int.Parse(Console.ReadLine());
+
+
+           // string destination = vacationType + 
+
+        }
+    }
+}
